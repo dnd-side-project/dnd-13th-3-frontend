79dc41580c5a534edcbe9625373f143808777b5c
Dohyeon <do.hy824@gmail.com>
2025-08-05 15:13:17 +0900

    ⚙️ Refactor: 폴더 구조 최적화

--- src/components/timer/TabSwitcher.tsx
+++ src/components/timer/TabSwitcher.tsx
@@ -5,14 +5,14 @@ export default function MainTabSwitcher() {
   const router = useRouter();
   const pathname = usePathname();
 
-  const isGoalActive = pathname === "/home";
-  const isTimerActive = pathname === "/home/timer";
+  const isGoalActive = pathname === "/main";
+  const isTimerActive = pathname === "/timer";
 
   return (
     <div className='flex justify-start items-center gap-3'>
       <button
         type='button'
-        onClick={() => router.push("/home")}
+        onClick={() => router.push("/main")}
         className={`text-heading-2 font-bold ${
           isGoalActive ? "text-gray-900" : "text-gray-400"
         }`}
@@ -21,7 +21,7 @@ export default function MainTabSwitcher() {
       </button>
       <button
         type='button'
-        onClick={() => router.push("/home/timer")}
+        onClick={() => router.push("/timer")}
         className={`text-heading-2 font-bold ${
           isTimerActive ? "text-gray-900" : "text-gray-400"
         }`}
