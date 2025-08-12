7c9fedd250132ddfe2edaf34aea9fe947f5f89e0
Dohyeon <do.hy824@gmail.com>
2025-08-12 22:04:27 +0900

    📋 Design: 텍스트 배치 수정

--- src/components/main/ProgressSection.tsx
+++ src/components/main/ProgressSection.tsx
@@ -2,15 +2,7 @@ import Image from "next/image";
 
 export default function ProgressSection() {
   return (
-    <div className="absolute justify-between items-center z-20 w-[303px] left-1/2 transform -translate-x-1/2">
-      <div className="flex justify-between items-center mb-2 mt-[243px]">
-        <div className="text-center justify-center text-gray-500 text-sm font-medium font-pretendard leading-tight tracking-tight">
-          치유중
-        </div>
-        <div className="text-center justify-center text-gray-500 text-sm font-medium font-pretendard leading-tight tracking-tight">
-          7시간
-        </div>
-      </div>
+    <div className="absolute justify-between items-center z-20 w-[303px] left-1/2 transform -translate-x-1/2 mt-[243px]">
       <div className="h-3 bg-white rounded-lg flex relative w-[303px]">
         <div className="w-1/2 h-3 bg-indigo-500 rounded-lg" />
         {/* 미누 캐릭터 */}
@@ -22,6 +14,14 @@ export default function ProgressSection() {
             height={51.5}
             priority
           />
+        </div>
+      </div>
+      <div className="flex justify-between items-center mt-2">
+        <div className="text-center justify-center text-gray-500 text-sm font-medium font-pretendard leading-tight tracking-tight">
+          치유중
+        </div>
+        <div className="text-center justify-center text-gray-500 text-sm font-medium font-pretendard leading-tight tracking-tight">
+          7시간
         </div>
       </div>
     </div>
