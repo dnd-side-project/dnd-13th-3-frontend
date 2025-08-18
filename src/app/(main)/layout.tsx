119a587b317ebdf356adaf66736db910d5ac5fd5
jiohjung98 <ckaquz98@naver.com>
2025-08-18 21:54:51 +0900

    🐛 design: 헤더 분기 처리 및 레이아웃 통일

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,14 +1,41 @@
+"use client";
+
 import { BottomNavbar, Logo } from "@/components";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
 
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const isChallengePage = pathname === "/challenge";
+
   return (
-    <div className='flex flex-col h-screen px-screen-margin'>
-      <div className='pt-4'>
-        <Logo />
+    <div className='flex flex-col'>
+      <div className={`pt-4 px-screen-margin ${isChallengePage ? 'bg-primary' : ''}`}>
+        {isChallengePage ? (
+          <div className="flex justify-between items-center">
+            <Image
+              src="/images/logos/MinuLogoWhite.svg"
+              alt="MINU Logo"
+              width={84}
+              height={24}
+              priority
+            />
+            <button className="">
+              <Image
+                src="/images/logos/Setting.svg"
+                alt="Settings"
+                width={24}
+                height={24}
+              />
+            </button>
+          </div>
+        ) : (
+          <Logo />
+        )}
       </div>
       <div className='flex-1 overflow-y-auto'>{children}</div>
       <BottomNavbar />
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as LoginLogo } from "./LoginLogo";
 export { default as Logo } from "./Logo";
 export { default as MinuCharacter } from "./MinuCharacter";
 export * from "./timer";
+export * from './challenge';
