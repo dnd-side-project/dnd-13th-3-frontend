36f9196565bbe41acf26c30628a24384d06b3732
jiohjung98 <ckaquz98@naver.com>
2025-08-22 14:21:16 +0900

    🐛 design: 메인 페이지 배경 이미지 및 통계 카드 반응형 처리

--- src/components/main/MainContent.tsx
+++ src/components/main/MainContent.tsx
@@ -73,12 +73,12 @@ export default function MainContent() {
 
         <div className='flex flex-col items-center justify-center relative mt-5'>
           {/* 배경 이미지 */}
-          <div className='justify-center items-center z-0'>
+          <div className='w-full min-h-96 relative z-0'>
             <Image
               src={backgroundImageSrc}
               alt='Screen Time Background'
-              width={335}
-              height={335}
+              fill
+              className='object-cover'
             />
           </div>
 
