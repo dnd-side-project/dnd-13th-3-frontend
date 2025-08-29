9d47ade9499c91185ede085de0e5d54ae21b23da
Dohyeon <do.hy824@gmail.com>
2025-08-29 23:41:57 +0900

    🎨Design: 요일별 텍스트 크기 수정

--- src/components/record/RecordClient.tsx
+++ src/components/record/RecordClient.tsx
@@ -487,7 +487,7 @@ export default function RecordClient({
                           />
                         </div>
                         <div
-                          className={`text-center text-caption-2 font-medium leading-none ${
+                          className={`text-center text-caption-1 font-medium leading-none ${
                             isSelected ? "text-gray-900" : "text-gray-400"
                           }`}
                         >
